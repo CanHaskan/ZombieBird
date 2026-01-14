e8690372253f5fdab1bab7088f6235ccdaad54ee
CanHaskan <canhaskan1903@hotmail.com>
2026-01-14 22:58:07 +0300

    Initial ZombieBird project

---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+
+    [SerializeField] private float objectSpeed = 1;
+    [SerializeField] private float resetPosition = -19.25f;
+    [SerializeField] private float startingPosition = 79.75f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (!GameManager.instance.GameOver)
+        {
+
+        
+        transform.Translate(Vector3.left * (objectSpeed*Time.deltaTime),Space.World);
+
+            if (transform.localPosition.x <= resetPosition)
+            {
+                Vector3 newPos = new Vector3(startingPosition, transform.position.y, transform.position.z);
+                transform.position = newPos;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : Object
+{
+    [SerializeField] Vector3 topPosition;
+    [SerializeField] Vector3 bottomPosition;
+    [SerializeField] float speed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Move(bottomPosition));
+
+    }
+    protected override void Update()
+    {
+        if (GameManager.instance.PlayerActive)
+        {
+            transform.Rotate(0, 100 * Time.deltaTime, 0);
+            base.Update();
+        }
+
+    }
+
+
+
+    IEnumerator Move(Vector3 target)
+    {
+        
+        while (Mathf.Abs((target - transform.localPosition).y) > 0.20f)
+        {
+            Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+            transform.localPosition += direction *speed* Time.deltaTime;
+
+            yield return null;
+        }
+
+
+
+
+       
+
+        yield return new WaitForSeconds(0.5f);
+
+        Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+
+        StartCoroutine(Move(newTarget));
+    }
+
+    
+
+        
+    
+}
